b2b0a85fc7af57bc084c6557c6cdc77a2a4f3ebd
Axel Calixte <axelcalixte@disroot.org>
2022-05-13 16:06:59 +0200

    script

---
+++ wifi-scan-if23.py
@@ -0,0 +1,42 @@
+import subprocess
+import time
+import json
+import re
+
+
+p = re.compile("^BSS ")
+
+
+def get_acquisitions():
+    """Returns array of dictionary entries, each one corresponding
+     to an AP """
+
+    result = subprocess.run(["sudo", "iw", "dev", "wlan0",
+                            "scan"], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8").split("\n")
+
+    signals = []
+    addr = []
+    for line in output:
+        if line.find("signal: ") != -1:
+            signals.append(float(line.split(" ")[1]))
+        if p.match(line) is not None:
+            # could be optimized by a better regex
+            addr.append(line.split(" ")[1].split("(")[0])
+
+    acquisitions = []
+    for line in zip(addr, signals):
+        dict = {"mac": line[0], "RSSI": line[1]}
+        acquisitions.append(dict)
+
+    return acquisitions
+
+
+dict = {}
+# number of acquisitions to get inside the range call
+for i in range(5):
+    dict[str(i)] = get_acquisitions()
+    time.sleep(3)
+
+with open("if23-feature.json", "w") as file:
+    json.dump(dict, file, indent=2)
